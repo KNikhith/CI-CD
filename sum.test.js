e4f4f69bac932a2abb4491e8e991811e2e91d46f
KNikhith <nikhithkotluri@gmail.com>
2025-10-17 21:41:51 +0530

    Sub Function

--- sum.test.js
+++ sum.test.js
@@ -1,4 +1,5 @@
 import sum from "./sum.js"
+import sub from "./sub.js"
 
 describe("test for sum function",()=>{
     test("adds 2+2 to equal 4",()=>{
@@ -7,4 +8,10 @@ describe("test for sum function",()=>{
     test("add -1 + -3 equal -4",()=>{
         expect(sum(-1,-3)).toBe(-4);
     })
+});
+
+describe("test for sub function",()=>{
+    test("sub of 5-2 equal to 3",()=>{
+        expect(sub(5,2)).toBe(3);
+    });
 })
